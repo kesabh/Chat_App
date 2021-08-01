00cbca1978b3c266ef896a3e3080915856491c53
Keshav Goel <goelkeshav1999@gmail.com>
2021-08-01 09:16:55 +0530

    package.json -  edited

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-const express = require("express") ; 
-const { Server } = require("socket.io") ; 
-const app = express() ; 
+const express = require("express");
+const { Server } = require("socket.io");
+const app = express();
 
-const http = require('http') ; 
-const server = http.createServer(app) ; 
+const http = require('http');
+const server = http.createServer(app);
 
-const io = new Server(server) ; 
+const io = new Server(server);
 
-let users = [ ] ; 
+let users = [];
 
-app.use(express.static("public")) ; 
+app.use(express.static("public"));
 
-io.on("connection" , function(socket){
-    
-    socket.on("userIsConnected", function(username){
-        let userObj = { id : socket.id , username : username } ; 
-        users.push( userObj ) ; 
+io.on("connection", function (socket) {
+
+    socket.on("userIsConnected", function (username) {
+        let userObj = { id: socket.id, username: username };
+        users.push(userObj);
         // console.log(users) ; 
 
-        socket.broadcast.emit("userJoined", userObj) ; 
+        socket.broadcast.emit("userJoined", userObj);
 
         // update other users list for self
-        socket.emit( "for-self" , users)
-        
-    }) ; 
+        socket.emit("for-self", users)
 
-    socket.on( "sendMessage", function(data){
-        socket.broadcast.emit( "sendMessage" , data ) ; 
-    } )
+    });
 
-    socket.on("disconnect", function(){
-        let userObj  ; 
-        let remainingUsers = users.filter(function(user){
-            if( user.id == socket.id ){
-                userObj = user ; 
-                return false ; 
+    socket.on("sendMessage", function (data) {
+        socket.broadcast.emit("sendMessage", data);
+    })
+
+    socket.on("disconnect", function () {
+        let userObj;
+        let remainingUsers = users.filter(function (user) {
+            if (user.id == socket.id) {
+                userObj = user;
+                return false;
             }
-            else{
-                return true ; 
+            else {
+                return true;
             }
-        }) ; 
+        });
 
-        users = remainingUsers ; 
-        socket.broadcast.emit("userLeft", userObj) ; 
+        users = remainingUsers;
+        socket.broadcast.emit("userLeft", userObj);
     })
 })
 
 
 
-let port = process.enc.PORT || 3003 ; 
-server.listen(port, function(){
-    console.log("Server started at port :  " + port ) ; 
-}) ; 
+let port = process.enc.PORT || 5500;
+server.listen(port, function () {
+    console.log("Server started at port :  " + port);
+});
 
 // app.get("/", function(request, response){
 //     response.send("Hello world ") ; 
